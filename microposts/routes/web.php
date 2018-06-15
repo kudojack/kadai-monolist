246afd77420a2f2ec5982798e3f6297954d8a172
EC2 Default User <ec2-user@ip-172-31-43-234.us-east-2.compute.internal>
2018-06-15 00:49:33 +0000

    post

--- microposts/routes/web.php
+++ microposts/routes/web.php
@@ -16,8 +16,14 @@ Route::get('/', function () {
 });
 // user registration
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::get('/', 'MicropostsController@index');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 // Login authentication
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
+
+});
